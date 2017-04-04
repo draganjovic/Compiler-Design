e175319aaac027f7b19c98cea2e5a71e893e1e77
Dragan Jović <dj47@zips.uakron.edu>
2017-04-04 01:59:35 -0400

    Add files via upload

--- Homework 3/parser.hpp
+++ Homework 3/parser.hpp
@@ -8,11 +8,11 @@
 #include "token.hpp"
 #include "statement.hpp"
 
-struct Parser {
+class Parser {
   private:
     std::vector<Token*> t;
     std::vector<Token*>::iterator it;
-    Context cxt;
+    Context* cxt;
 
     // return if there are no more tokens
     bool Eof() { return it == t.end(); }
@@ -38,6 +38,40 @@ struct Parser {
     // require that token have given kind & consume
     Token * Require(Token_kind k);
 
-        
+    // Parse functions
+    Expr * ParseExpr();      // expression
+    Expr * ParseCond();      // conditional expression
+    Expr * ParseOr();        // logical or expression
+    Expr * ParseAnd();       // logical and expression
+    Expr * ParseEqual();     // equality expression
+    Expr * ParseOrdering();  // relational expression
+    Expr * ParseAdd();       // additive expression
+    Expr * ParseMult();      // multiplicative expression
+    Expr * ParseUnary();     // unary expression
+    Expr * ParsePrimary();   // boolean and integer literals
 
-}
+    // Statements
+    Statement * ParseStmt();       // statement
+    Statement * ParseDecl_Stmt();  // declaration statement
+    Statement * ParseExpr_Stmt();  // expression statement
+
+    // Declarations
+    Declaration * ParseDecl();
+    Declaration * ParseVariable_Decl();
+
+    // Types
+    const Type * ParseType();
+
+  public:
+    Statement * Parse() { return ParseStmt(); }
+
+    // Constructor
+    Parser(std::vector<Token*> _t, Context* _cxt) : t(_t), cxt(_cxt)
+    {
+      it = t.begin();
+    }
+
+    // destructor
+    ~Parser() {}
+    
+  }
