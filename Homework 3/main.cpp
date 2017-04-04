1e0a719e1cd1251dd67e7e91479eea34af306194
Dragan Jović <dj47@zips.uakron.edu>
2017-04-04 11:01:50 -0400

    Create main.cpp

---
+++ Homework 3/main.cpp
@@ -0,0 +1,27 @@
+#include "ast.hpp"
+#include "evaluate.hpp"
+#include "print.hpp"
+#include "verify_new.hpp"
+#include "token.hpp"
+#include "parser.hpp"
+#include "lexer.hpp"
+#include <iostream>
+
+int main() {
+    Context context_;
+    {
+      Expr* e = new multi_expr(new int_expr(5), new int_expr(2)); // 5 * 2
+      check(context_, e); // check function from "verify_new.hpp"
+      print(e); // print function from "print.hpp"
+      std::cout << " == " << eval(e).data.b << std::endl;
+    }
+  }
+
+    std::vector<Token*> t;
+    Token* tok = new Token();
+
+// parse tokens
+      Parser * parser = new Parser(*t, cxt);
+      cout << parser;
+
+}
