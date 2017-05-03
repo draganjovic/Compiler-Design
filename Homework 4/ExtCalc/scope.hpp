3c1e7b71bf71b5a0df3e63e73e073a0715580362
Dragan Jović <dj47@zips.uakron.edu>
2017-05-03 01:53:34 -0400

    using Dr.Suttons example

---
+++ Homework 4/ExtCalc/scope.hpp
@@ -0,0 +1,23 @@
+#include "symbols.hpp"
+
+#include <unordered_map>
+
+struct context;
+struct token;
+struct type;
+struct expr;
+struct Declaration;
+struct Statememt;
+
+/// A binding environment associates symbols with their declarations.
+struct environment
+{
+  /// Register a new binding between a name and its declaration.
+  void insert(symbol*, decl*);
+
+  /// Lookup the current binding for a symbol. Returns nullptr if the symbol
+  /// is unbound.
+  decl* find(symbol*);
+
+  std::unordered_map<symbol*, decl*> map;
+};
