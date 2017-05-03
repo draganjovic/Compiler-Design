e2fe75e9fa7934f98287b6c6a941fa5f62302232
Dragan Jović <dj47@zips.uakron.edu>
2017-05-02 23:00:16 -0400

    Create type.hpp

---
+++ Homework 4/ExtCalc/type.hpp
@@ -0,0 +1,23 @@
+#ifndef TYPE_HPP
+#define TYPE_HPP
+
+#include "visitor.hpp"
+
+
+struct type
+{
+    virtual ~type() {}
+    virtual void accept(type_visitor &) const = 0;
+};
+
+struct int_type : type
+{
+    void accept(type_visitor & v) const { v.visit(this); }
+};
+
+struct bool_type : type
+{
+    void accept(type_visitor & v) const { v.visit(this); }
+};
+
+#endif
